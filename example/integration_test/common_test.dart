b2446bd60ef166cae91b44d6ab0f3c49aee4d524
xiaoweiguoyuan <1019713654@qq.com>
2023-04-28 17:50:05 +0800

    更新drawer、fab、detail、appbar等测试case

--- example/integration_test/common_test.dart
+++ example/integration_test/common_test.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:example/main.dart' as app;
-import 'integration_test_util.dart' as util;
+import 'integration_test_data.dart';
+import 'integration_test_util.dart';
 
 void main() {
 
@@ -44,8 +41,6 @@ void main() {
     //PageView
     // var jsonArray = '[{"action":"delayed"},{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"pump"},{"action":"find.text","text":"fair使用和介绍"},{"action":"expect","expect":1},{"action":"find.text","text":"fair 模板代码"},{"action":"expect","expect":1},{"action":"tap"},{"action":"pumpAndSettle"},{"action":"pump"},{"action":"delayed"},{"action":"find.text","text":"PageView >>>"},{"action":"expect","expect":1},{"action":"tap"},{"action":"pumpAndSettle"},{"action":"pump"},{"action":"delayed"},{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"pump"},{"action":"delayed"},{"action":"takeScreenshot"},{"action":"delayed"},{"action":"find.text","text":"click show message!"},{"action":"expect","expect":0}]';
 
-    // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"find.text", "text":"appbar_template入口"},{"action":"tap"},{"action":"find.text", "text":"AppBar"},{"action":"takeScreenshot"}]';
-    // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"find.text", "text":"detail_page入口"},{"action":"expect","expect":1},{"action":"tap"}]';
     // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"find.byType", "type":"ListView"},{"action":"drag","offsetY":-600.0},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text":"drawer_template入口"},{"action":"expect","expect":1}]';
     //     '{"action":"delayed"},{"action":"pump"},{"action":"find.byType","type":"Drawer"},{"action":"tap"}, '
     //     '{"action":"delayed"},{"action":"pump"},{"action":"find.byType","type":"ListView"},'
@@ -54,9 +49,23 @@ void main() {
     //     '{"action":"find.text","text":"位置"},{"action":"expect","expect":1},'
     //     '{"action":"find.text","text":"设置"},{"action":"expect","expect":1},'
     //     '{"action":"find.byType","type":"Drawer"},{"action":"tap"},]';
-    var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"find.byType", "type":"ListView"},{"action":"drag","offsetY":-600.0},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text":"fab_template入口"},{"action":"expect","expect":1},{"action":"tap"},'
-        '{"action":"delayed"},{"action":"pumpAndSettle"}, {"action":"takeScreenshot"}]';
-    await util.integrationTestByJson( tester, binding,jsonArray);
+    // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"find.byType", "type":"ListView"},{"action":"drag","offsetY":-600.0},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text":"fab_template入口"},{"action":"expect","expect":1},{"action":"tap"},'
+    //     '{"action":"delayed"},{"action":"pumpAndSettle"}, {"action":"takeScreenshot"}]';
+    // await util.integrationTestByJson( tester, binding,jsonArray);
+    //appbar_template tests
+    // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"delayed"},{"action":"find.text", "text": "fair 模板代码","expect":1},{"action":"expect","expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text": "AppBar >>>"},{"action":"expect", "expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"takeScreenshot"},{"action":"find.text","text":"click show message!"},{"action":"expect","expect":0}]';
+    //detail_page tests
+    // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"find.text", "text": "fair 模板代码","expect":1},{"action":"expect","expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"find.byType","type":"ListView"},{"action":"expect","expect":1},{"action":"drag","offsetY":-1000.0},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text": "Detail >>>"},{"action":"expect", "expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"takeScreenshot"},{"action":"find.text","text":"click show message!"},{"action":"expect","expect":0}]';
+    //FAB tests
+    // var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"delayed"},{"action":"find.text", "text": "fair 模板代码","expect":1},{"action":"expect","expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text": "FAB >>>"},{"action":"expect", "expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"takeScreenshot"},{"action":"find.text","text":"click show message!"},{"action":"expect","expect":0}]';
+    //Drawer tests
+    var jsonArray = '[{"action":"delayed"},{"action":"pumpAndSettle"},{"action":"delayed"},{"action":"find.text", "text": "fair 模板代码","expect":1},{"action":"expect","expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"find.text", "text": "Drawer >>>"},{"action":"expect", "expect":1},{"action":"tap"},{"action":"delayed"},{"action":"pump"},{"action":"takeScreenshot"},{"action":"find.text","text":"click show message!"},{"action":"expect","expect":0}]';
+    await integrationTestByJson( tester, binding,jsonArray);
+
+
+    List<ConfigData> dataList= [ConfigData.delayed(),ConfigData.delayed(),ConfigData.pumpAndSettle(),ConfigData.pump(),ConfigData.findText('fair使用和介绍'),ConfigData.expect(1),ConfigData.findText('fair 模板代码'),ConfigData.expect(1),ConfigData.tap(),ConfigData.pumpAndSettle(),ConfigData.pump(),ConfigData.delayed(),ConfigData.findText('StaggeredView >>>'),ConfigData.expect(1),ConfigData.tap(),ConfigData.pumpAndSettle(),ConfigData.pump(),ConfigData.delayed(),ConfigData.delayed(),ConfigData.pumpAndSettle(),ConfigData.pump(),ConfigData.takeScreenshot(),ConfigData.findText('click show message!'),ConfigData.expect(0)];
+
+    // await integrationTestByDataList(tester, binding, dataList);
   });
 
 }
--- example/integration_test/integration_test_util.dart
+++ example/integration_test/integration_test_util.dart
@@ -4,29 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-class ConfigData extends Object {
-  // 事件类型  pumpAndSettle（刷新跳转页面） 、pump（刷新页面）、tap（点击）、enterText（输入文字 必须有text）、drag（拖动必须有 offsetX 、offsetX）、find.text（文字查找 必须有text）、find.byKey（查找元素 必须有key）、find.byType（查找元素 必须有type）、expect（预期 必须有expect ）、takeScreenshot （截屏）、delayed （延时）
-  String action = '';
-
-  // key
-  String key = '';
-
-  // 元素类型  （Image、Text、Icon、TextField、ListView）
-  String type = '';
-
-  // 文字
-  String text = '';
-
-  // 期望数量  _FindsWidgetMatcher(null, 0)
-  int expect = 0;
-
-  // drag offset.dx
-  double offsetX = 0.0;
-
-  // drag offset.dy
-  double offsetY = 0.0;
-}
-
+import 'integration_test_data.dart';
 
 Future<Finder?> commonTestByConfigData(
     ConfigData configData,
@@ -84,6 +62,12 @@ Future<Finder?> commonTestByConfigData(
         case 'TextField':
           finder = find.byType(TextField);
           break;
+        case 'ListTile':
+          finder = find.byType(ListTile);
+          break;
+        case 'Drawer':
+          finder = find.byType(Drawer);
+          break;
         default:
           print("暂不支持 type " + configData.type);
           break;
@@ -124,7 +108,6 @@ Future<Finder?> commonTestByConfigData(
 Future<void> delayed() async {
   await Future.delayed(const Duration(milliseconds: 2000)).then((value) {
     // ignore: avoid_print
-    print("延时2秒执行 then ");
   });
 }
 
@@ -139,28 +122,7 @@ Future<void> takeScreenshot(
 
 Future<Finder?> commonTest(Map userMap, WidgetTester tester,
     IntegrationTestWidgetsFlutterBinding binding, Finder? finder) async {
-  var configData = ConfigData();
-  if (userMap['action'] != null) {
-    configData.action = userMap['action'];
-  }
-  if (userMap['type'] != null) {
-    configData.type = userMap['type'];
-  }
-  if (userMap['text'] != null) {
-    configData.text = userMap['text'];
-  }
-  if (userMap['expect'] != null) {
-    configData.expect = userMap['expect'];
-  }
-  if (userMap['offsetX'] != null) {
-    configData.offsetX = userMap['offsetX'];
-  }
-  if (userMap['offsetY'] != null) {
-    configData.offsetY = userMap['offsetY'];
-  }
-
-  print(userMap.toString());
-  return commonTestByConfigData(configData, tester, binding, finder);
+  return commonTestByConfigData(ConfigData.fromMap(userMap), tester, binding, finder);
 }
 
 
